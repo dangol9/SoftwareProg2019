03baa7b92979c28a8bb2739b22ef1f70fcd600d7
dangol9 <dango991@gmail.com>
2019-09-09 14:44:00 +0300

    ..

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = 3000
 const path = require("path");
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, "web_store", "index.html"));
 })
 app.use(express.static('web_store'));
-app.listen(process.env.port || port, () => {
-  console.log("Server started", port);
+app.listen(process.env.PORT || PORT, () => {
+  console.log("Server started", PORT);
 });
