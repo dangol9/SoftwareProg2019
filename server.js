7e08392b41b858cfd93717cebb939790df8997cd
dangol9 <dango991@gmail.com>
2019-09-09 14:40:35 +0300

    .

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, "web_store", "index.html"));
 })
 app.use(express.static('web_store'));
-app.listen(process.env.PORT || PORT, () => {
-  console.log("Server started", PORT);
+app.listen(process.env.port || port, () => {
+  console.log("Server started", port);
 });
